daac8e61d576c8964b1a81830faef07e7b68e2a0
Jim Rennie <JLRennie@gmail.com>
2020-11-14 20:52:09 -0600

    low risk zones
    probably won't use it

---
+++ jazz_snake/layer/lowriskzoneslayer.py
@@ -0,0 +1,20 @@
+from jazz_snake.board.layerlifecycle import LayerLifeCycle
+from jazz_snake.board.celldatatype import CellDataType
+from jazz_snake.board.deaththreatlevel import DeathThreatLevel
+from jazz_snake.board.gameboard import GameBoard
+
+
+class LowRiskZonesLayer:
+    
+    LIFE_CYCLE = LayerLifeCycle.DEATH_THREAT_ANALYSIS
+
+    def visit(self, game_board: GameBoard):
+        min_width = 0
+        max_width = game_board.get_width()
+        min_height = 0
+        max_height = game_board.get_height()
+        for x in range(min_width, max_width):
+            for y in range(min_height, max_height):
+                if x in (min_width, min_width + 1, max_width - 1, max_width) \
+                        or y in (min_width, min_width + 1, max_width - 1, max_width):
+                    game_board.set_cell(x, y, CellDataType.DEATH_THREAT_LEVEL, DeathThreatLevel.SMALL)
