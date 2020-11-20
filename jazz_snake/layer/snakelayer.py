4aa4a2a4da4b333732141603585bb58f3b53243f
Jim Rennie <JLRennie@gmail.com>
2020-11-20 01:16:52 -0600

    remove danger level from snek butt

--- jazz_snake/layer/snakelayer.py
+++ jazz_snake/layer/snakelayer.py
@@ -30,7 +30,7 @@ class SnakeLayer:
         #     game_board.set_cell(head_x, head_y - 1, CellDataType.DEATH_THREAT_LEVEL, DeathThreatLevel.EXTREME)
 
         snake_body = self._snake['body']
-        for i in range(self._snake['length'] - 1):
+        for i in range(self._snake['length'] - 2):
             game_board.set_cell(
                 snake_body[i]['x'],
                 snake_body[i]['y'],
